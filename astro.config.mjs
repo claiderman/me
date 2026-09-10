9dc4398dd1d8d368b5f7307de0b58d27d9f24915
Claudio Lozano <claiderman.lozano@gmail.com>
2026-09-09 20:03:58 -0500

    feat: add path aliases for improved module resolution and update components with new imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,37 @@
 import { defineConfig } from "astro/config";
 import astroI18next from "astro-i18next";
 
+const paths = {
+  "@components/*": "src/components/*",
+  "@accordion/*": "src/components/accordion/*",
+  "@buttons/*": "src/components/button/*",
+  "@card/*": "src/components/card/*",
+  "@client/*": "src/components/client/*",
+  "@lists/*": "src/components/lists/*",
+  "@modal/*": "src/components/modal/*",
+  "@sections/*": "src/components/sections/*",
+  "@theme/*": "src/components/theme/*",
+  "@yearBadge/*": "src/components/yearBadge/*",
+  "@experience/*": "src/components/sections/experience/*",
+  "@education/*": "src/components/sections/education/*",
+  "@navbar/*": "src/components/navbar/*",
+  "@layouts/*": "src/layouts/*",
+  "@types/*": "src/types/*",
+  "@data/*": "src/data/*",
+  "@utils/*": "src/utils/*",
+  "@scripts/*": "src/scripts/*",
+  "@i18n/*": "src/i18n/*",
+  "@icons/*": "src/icons/*",
+  "@globalStyles/*": "src/styles/global.css"
+};
+
 export default defineConfig({
   site: "https://claiderman.github.io",
   base: "/me",
+  vite: {
+    resolve: {
+      alias: paths
+    }
+  },
   integrations: [astroI18next()],
 });
